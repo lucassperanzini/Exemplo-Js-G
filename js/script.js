441c90542a24cad8fb8dd39eb09c4ae6632f5b85
lucassperanzini <RM98297@fiap.com.br>
2023-03-07 11:49:01 -0300

    script.js adicionado ao HTML

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Este é um comentário de linha
+
+/**
+ * é um comentário
+ * de
+ * bloco
+ */
+
+console.log("Olá mundo!")
+console.error("Bom dia!")
+console.warn("Será?")
+console.info("Vai ser....")
